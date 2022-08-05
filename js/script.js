14a0adee5c0a962ff95191d5c445fb0eebdcd522
Ahmad Daud <ahmaddaudbaloch@gmail.com>
2022-08-06 02:50:55 +0500

    Made cleaner code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+let playerSelection = prompt("Rock, Paper or Scissors?");
+playerSelection = playerSelection.toUpperCase();
 function getComputerChoice() {
     let computerSelection;
     let RandomChoice = Math.floor(Math.random() * 11) -5;
@@ -12,30 +14,35 @@ function getComputerChoice() {
     };
     return computerSelection;
 }
-
+const computerSelection = getComputerChoice(); 
 function RockPaperScissors(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    console.log(`Player: ${playerSelection}`);
-    console.log(`Computer: ${computerSelection}`);    
+    let Gameresult;
     if (playerSelection === computerSelection) {
-        console.log('Its a draw!');
+        Gameresult = 0;
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
-        console.log('SCISSORS beats PAPER');
-        console.log('Player wins!');
+        Gameresult = 1;
     } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
-        console.log('SCISSORS beats PAPER');
-        console.log('Computer wins!');
+        Gameresult = 2;
     } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
-        console.log('PAPER beats ROCK');
-        console.log('Player wins!');
+        Gameresult = 1;
     } else if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
-        console.log('PAPER beats ROCK');
-        console.log('Computer wins!');
+        Gameresult = 2;
     } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
-        console.log('ROCK beats SCISSORS');
-        console.log('Player wins!');
+        Gameresult = 1;
     } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
-        console.log('ROCK beats SCISSORS');
-        console.log('Computer wins!');
+        Gameresult = 2;
+    return Gameresult;
+}
+}
+function game() {
+    for (let n = 0; n < 5; n++) {
+        RockPaperScissors(playerSelection, computerSelection);
+        if (Gameresult == 1) {
+            console.log('Player Wins!');
+        } else if (Gameresult == 2) {
+            console.log('Computer Wins!');
+        } else if (GameResult == 0) {
+            console.log('Its A draw!');
+        }
     }
 }
