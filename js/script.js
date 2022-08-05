d4221898ae8fac58480585aa5d437886f81e4cbd
Ahmad Daud <ahmaddaudbaloch@gmail.com>
2022-08-06 01:40:52 +0500

    Renamed variables in accordance with the project

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 function getComputerChoice() {
-    let ComputerChoice;
+    let computerSelection;
     let RandomChoice = Math.floor(Math.random() * 11) -5;
     if (RandomChoice < 0) { RandomChoice = RandomChoice * (-1) - 1}; 
     if (RandomChoice == 0) { RandomChoice = RandomChoice + 5};
     if (RandomChoice == 5) {
-        ComputerChoice = 'Rock';
+        computerSelection = 'Rock';
     } else if (RandomChoice == 3 || RandomChoice == 4) {
-        ComputerChoice = 'Paper';
+        computerSelection = 'Paper';
     } else if (RandomChoice == 1 || RandomChoice == 2) {
-        ComputerChoice = 'Scissors';
+        computerSelection = 'Scissors';
     };
-    return ComputerChoice;
+    return computerSelection;
 }
